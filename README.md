2f0d782cfae3af7f7b1d25b6f0b6be4eafc72eba
azu <azuciao@gmail.com>
2019-07-07 12:30:26 +0900

    init

---
+++ README.md
@@ -0,0 +1,65 @@
+# @textlint/migrate-textlint-scripts-typescript
+
+Migrate [textlint-scripts](https://github.com/textlint/textlint-scripts) to TypeScript version.
+
+## Target
+
+- The user use [textlint-scripts](https://github.com/textlint/textlint-scripts) with JavaScript
+
+## What do migration script?
+
+- Setup [textlint-scripts](https://github.com/textlint/textlint-scripts) with TypeScript
+- Install TypeScript DevDependencies
+- Setup TypeScript Configuration
+
+## After Migration
+
+You should migrate JavaScript Code to TypeScript code manually.
+
+## Install
+
+Install with [npm](https://www.npmjs.com/):
+
+    npm install @textlint/migrate-textlint-scripts-typescript -g
+
+## Usage
+
+    Usage
+      $ npx migrate-textlint-scripts-typescript
+ 
+    Options
+      --yarn  Use yarn if it is specified
+ 
+    Examples
+      $ npx migrate-textlint-scripts-typescript --yarn
+
+## Changelog
+
+See [Releases page](https://github.com/textlint/migrate-textlint-scripts-typescript/releases).
+
+## Running tests
+
+Install devDependencies and Run `npm test`:
+
+    npm test
+
+## Contributing
+
+Pull requests and stars are always welcome.
+
+For bugs and feature requests, [please create an issue](https://github.com/textlint/migrate-textlint-scripts-typescript/issues).
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Author
+
+- [github/azu](https://github.com/azu)
+- [twitter/azu_re](https://twitter.com/azu_re)
+
+## License
+
+MIT © azu
---
+++ bin/migrate-textlint-scripts.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const shelljs = require("shelljs");
+const npeBin = require.resolve(".bin/npe");
+const meow = require('meow');
+const cli = meow(`
+    Usage
+      $ npx migrate-textlint-scripts-typescript
+ 
+    Options
+      --yarn  Use yarn if it is specified
+ 
+    Examples
+      $ npx migrate-textlint-scripts-typescript --yarn
+`, {
+    flags: {
+        yarn: {
+            type: 'boolean'
+        }
+    }
+});
+const USE_YARN = cli.flags.yarn;
+const log = function (message) {
+    console.info(message);
+};
+const exec = function (command) {
+    log("Run: " + command);
+    if (shelljs.exec(command).code !== 0) {
+        throw new Error("Fail to execute:" + command);
+    }
+};
+const npe = function (key, value) {
+    exec(`${npeBin} "${key}" "${value}"`);
+};
+// Exist config files
+const mochaOptPath = path.join(process.cwd(), "test", "mocha.opts");
+log(`Package Manager: ${USE_YARN ? "yarn" : "npm"}`);
+const INSTALL_COMMAND = USE_YARN ? "yarn install --dev" : "npm install --save-dev";
+const UNINSTALL_COMMAND = USE_YARN ? "yarn remove --dev" : "npm uninstall --save-dev";
+// Install textlint-scripts
+exec(`${INSTALL_COMMAND} typescript ts-node @textlint/types`);
+// Modify package.json
+npe("scripts.build", "textlint-scripts build");
+npe("scripts.watch", "textlint-scripts build --watch");
+npe("scripts.test", "textlint-scripts test");
+// Modify mocha.opts
+if (fs.existsSync(mochaOptPath)) {
+    const mochaOptsContent = fs.readFileSync(mochaOptPath, "utf-8");
+    const replaced = mochaOptsContent
+        .replace("--compilers js:babel-register", "--require textlint-scripts/register-ts")
+        .replace("--compilers js:@babel/register", "--require textlint-scripts/register-ts")
+        .replace("--require babel-register", "--require textlint-scripts/register-ts")
+        .replace("--require @babel/register", "--require textlint-scripts/register-ts")
+        .replace("--require textlint-scripts/register", "--require textlint-scripts/register-ts");
+    fs.writeFileSync(mochaOptPath, replaced, "utf-8");
+    log(`✔ Rewrite ${mochaOptPath}`);
+}
+// Complete
+log("✔ Complete!");
